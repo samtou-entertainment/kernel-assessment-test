8de06b379f7de852d7fd558d0a8c01b732e93238
Samtou Christophe ASSEKOUDA <christophe1999da@gmail.com>
2022-11-27 11:48:14 +0000

    env var

--- src/config/dev.js
+++ src/config/dev.js
@@ -8,8 +8,4 @@ export const config = {
   dbUrl:
     process.env.CONNECTIONSTRING ||
     'mongodb://localhost:27017/vaillant-batisseur', // 'mongodb://DESKTOP-7F87LGU:27017,DESKTOP-7F87LGU:27018,DESKTOP-7F87LGU:27019/vaillant-batisseur?replicaSet=rs'
-  cinetpay_api_key: process.env.CINETPAY_API_KEY,
-  cinetpay_site_id: process.env.CINETPAY_SITE_ID,
-  admin_id: process.env.ADMIN_ID,
-  anonymous_id: process.env.ANONYMOUS_ID
 }
