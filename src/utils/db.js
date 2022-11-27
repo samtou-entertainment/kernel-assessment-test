c8338ae5159fb2d382a7d010c484ecf2d024e41d
Samtou Christophe ASSEKOUDA <christophe1999da@gmail.com>
2022-11-27 11:43:07 +0000

    test

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,5 +2,6 @@ import mongoose from 'mongoose'
 import options from '../config'
 
 export const connect = (url = options.dbUrl, opts = {}) => {
+  console.log(url);
   return mongoose.connect(url, { ...opts, useNewUrlParser: true })
 }
